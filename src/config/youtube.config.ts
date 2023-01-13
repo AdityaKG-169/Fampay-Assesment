56ed492af45fbd6a880d8270812ad112ab0e3f9b
AdityaKG-169 <aditya.kg169@gmail.com>
2023-01-14 01:33:26 +0530

    feat: Config for YouTube API Calls
    Wrote configuration that would be used to make Youtube API Calls.

---
+++ src/config/youtube.config.ts
@@ -0,0 +1,18 @@
+// The .env file contains YOUTUBE_API_KEYS=API_KEY_1,API_KEY_2,API_KEY_3. The API keys are separated by commas.
+const apiKeysUnparsed = process.env.YOUTUBE_API_KEYS;
+
+if (!apiKeysUnparsed) {
+	throw new Error('No YouTube API keys found');
+}
+
+// Split the unparsed API keys into an array of strings (API keys) using the comma as the separator character (',').
+const apiKeys = apiKeysUnparsed.split(',');
+
+const youtubeConfig = {
+	apiKeys,
+	maxResultsPerRequest: 5, // The maximum number of results that can be returned in a single request.
+	searchQuery: 'music', // The search query to use when searching for videos.
+	initialPublishedAfter: '2023-01-01T00:00:00Z', // The date after which videos should be published. To avoid very old videos from the cache.
+};
+
+export default youtubeConfig;
