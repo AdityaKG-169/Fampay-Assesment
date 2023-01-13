824e1d519c0e01fa3d05ac99c5f2cc05a9f7664c
AdityaKG-169 <aditya.kg169@gmail.com>
2023-01-14 02:41:13 +0530

    feat: Function to GET paginated videos
    Created function in the data access layer file of the videos that could get multiple videos, paginated, and in the descending order of 'publishedAt' key.

--- src/components/videos/dal.videos.ts
+++ src/components/videos/dal.videos.ts
@@ -37,6 +37,48 @@ const saveMultipleVideos = async (videos: TVideo[]) => {
 	}
 };
 
-export default {
-	saveMultipleVideos,
+// Returns the stored video data in a paginated response sorted in descending order of publishedAt.
+// Page number starts from 1.
+const getPaginatedVideos = async (page: number) => {
+	try {
+		const limit = 10;
+
+		const videos = await Video.find()
+			.sort({ publishedAt: -1 })
+			.skip((page - 1) * limit)
+			.limit(limit)
+			.exec();
+
+		if (videos && videos.length > 0) {
+			const responseObject: TServerResponse = {
+				type: 'success',
+				status: 200,
+				message: 'Videos fetched successfully',
+				data: videos,
+				uniqueCode: 'VIDEOS_FETCHED',
+			};
+			return responseObject;
+		}
+
+		const responseObject: TServerResponse = {
+			type: 'error',
+			status: 404,
+			message: 'Videos not found',
+			data: null,
+			uniqueCode: 'VIDEOS_NOT_FOUND',
+		};
+
+		return responseObject;
+	} catch (error) {
+		const responseObject: TServerResponse = {
+			type: 'error',
+			status: 500,
+			message: 'Internal server error (getPaginatedVideos)',
+			data: null,
+			uniqueCode: 'INTERNAL_SERVER_ERROR',
+		};
+		return responseObject;
+	}
 };
+
+export { saveMultipleVideos, getPaginatedVideos };
