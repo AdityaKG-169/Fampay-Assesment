5918935abb213e594a055095673cd0c5c0e192de
AdityaKG-169 <aditya.kg169@gmail.com>
2023-01-17 17:34:23 +0530

    refactor: add options to mongodb connection
    Had to add options to mongodb connection because of outdated version of mongoose in package.json

--- src/config/mongoose.config.ts
+++ src/config/mongoose.config.ts
@@ -3,7 +3,11 @@ import { set, connect, connection } from 'mongoose';
 import serverConfig from './server.config';
 
 set('strictQuery', true);
-connect(serverConfig.mongoUri);
+connect(serverConfig.mongoUri, {
+	useCreateIndex: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 const db = connection;
 
 export default db;
