9fa3b0357485198408c224b70867445b52e54b0f
AdityaKG-169 <aditya.kg169@gmail.com>
2023-01-19 00:38:33 +0530

    feat: add sortBy to searchVideos and getPaginatedVideos
    Added functionality to sort videos by their published date in ascending or descending order.

--- src/components/videos/dal.videos.ts
+++ src/components/videos/dal.videos.ts
@@ -39,12 +39,24 @@ const saveMultipleVideos = async (videos: TVideo[]) => {
 
 // Returns the stored video data in a paginated response sorted in descending order of publishedAt.
 // Page number starts from 1.
-const getPaginatedVideos = async (page: number) => {
+// sortBy: asc or desc
+const getPaginatedVideos = async (page: number, sortBy: string) => {
 	try {
 		const limit = 10;
 
+		if (sortBy !== 'asc' && sortBy !== 'desc') {
+			const responseObject: TServerResponse = {
+				type: 'error',
+				status: 400,
+				message: 'Invalid sort order',
+				data: null,
+				uniqueCode: 'INVALID_SORT_ORDER',
+			};
+			return responseObject;
+		}
+
 		const videos = await Video.find()
-			.sort({ publishedAt: -1 })
+			.sort({ publishedAt: sortBy === 'asc' ? 1 : -1 })
 			.skip((page - 1) * limit)
 			.limit(limit)
 			.exec();
@@ -83,12 +95,24 @@ const getPaginatedVideos = async (page: number) => {
 
 // Function to return the video whose title or description matches the search query.
 // Page number starts from 1.
-const searchVideos = async (query: string, page: number) => {
+// sortBy: asc or desc
+const searchVideos = async (query: string, page: number, sortBy: string) => {
 	try {
 		const limit = 10;
 
+		if (sortBy !== 'asc' && sortBy !== 'desc') {
+			const responseObject: TServerResponse = {
+				type: 'error',
+				status: 400,
+				message: 'Invalid sort order',
+				data: null,
+				uniqueCode: 'INVALID_SORT_ORDER',
+			};
+			return responseObject;
+		}
+
 		const videos = await Video.fuzzySearch(query)
-			.sort({ publishedAt: -1 })
+			.sort({ publishedAt: sortBy === 'asc' ? 1 : -1 })
 			.skip((page - 1) * limit)
 			.limit(limit)
 			.exec();
