3ad362e822a976e3effbeeebda63d59f09665a34
AdityaKG-169 <aditya.kg169@gmail.com>
2023-01-26 20:37:56 +0530

    feat: add controller for the network requests
    add controllers to access video dals via network requests.

---
+++ src/components/videos/controllers.videos.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+import { searchVideos, getPaginatedVideos } from './dal.videos';
+
+interface IQueryAndRequest extends Request {
+	query: {
+		query: string | undefined;
+		page: string | undefined;
+		sortBy: 'asc' | 'desc' | undefined;
+	};
+}
+
+const getPaginatedVideosController = async (
+	req: IQueryAndRequest,
+	res: Response
+) => {
+	const { page, sortBy, query } = req.query;
+
+	if (!query || !query.trim()) {
+		const paginatedVideos = await getPaginatedVideos(
+			parseInt(page as string, 10),
+			sortBy as string
+		);
+
+		return res.status(paginatedVideos.status).json(paginatedVideos);
+	}
+
+	const searchResults = await searchVideos(
+		query,
+		parseInt(page as string, 10),
+		sortBy as string
+	);
+
+	return res.status(searchResults.status).json(searchResults);
+};
+
+export default getPaginatedVideosController;
